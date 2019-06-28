204179f262f13328be93905cde397a14db112397
Patlu <47553861+maximsukhorukov@users.noreply.github.com>
2019-06-28 13:25:31 +0200

    Add files via upload
    Raumschiff kann sich vollständig nach Rechts und nach Links bewegen mit 20px/tick;

--- Form1.cs
+++ Form1.cs
@@ -13,7 +13,10 @@ namespace Space_Invaders
     public partial class Form1 : Form
     {
         Rectangle Raumschiff;
-        bool RaumschiffBewegen;
+        bool RaumschiffBewegenLinks = false;
+        bool RaumschiffBewegenRechts = false;
+        int xRaumschiff = 35;
+        int yRaumschiff = 35;
 
         public Form1()
         {
@@ -29,17 +32,22 @@ namespace Space_Invaders
         }
         private void Timer_tick(object sender, EventArgs e)
         {
-            if (RaumschiffBewegen = true)
+            if (RaumschiffBewegenLinks)
+            {
+                xRaumschiff += 20;
+            }
+
+            if (RaumschiffBewegenRechts)
             {
                 xRaumschiff -= 20;
             }
-                
+
+
 
             Invalidate();
         }
 
-        int xRausmchiff = 35;
-        int yRaumschiff = 35;
+
         Timer timer;
 
         public void MoveRaumschiff()
@@ -51,7 +59,7 @@ namespace Space_Invaders
             Raumschiff = new Rectangle();
             Pen stiftSchwarz = new Pen(Color.Black);
             Graphics graphics = e.Graphics;
-            graphics.DrawRectangle(stiftSchwarz, this.ClientSize.Width / 2 - xRausmchiff / 2, this.ClientSize.Height - 50, 50, 50);
+            graphics.DrawRectangle(stiftSchwarz, this.ClientSize.Width / 2 - xRaumschiff / 2, this.ClientSize.Height - 50, 50, 50);
             base.OnPaint(e);
         }
         private void Form1_Load(object sender, EventArgs e)
@@ -62,14 +70,10 @@ namespace Space_Invaders
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.A || e.KeyCode == Keys.Left)
-            {
-                xRausmchiffBewegen = true;
-                xRausmchiff = xRausmchiff + 10;
-            }
+                RaumschiffBewegenLinks = true;
+
             if (e.KeyCode == Keys.D | e.KeyCode == Keys.Right)
-            {
-                xRausmchiff--;
-            }
+                RaumschiffBewegenRechts = true;
         }
 
         private void Form1_KeyPress(object sender, KeyPressEventArgs e)
@@ -79,7 +83,11 @@ namespace Space_Invaders
         }
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
+            if (e.KeyCode == Keys.A || e.KeyCode == Keys.Left)
+                RaumschiffBewegenLinks = false;
 
+            if (e.KeyCode == Keys.D | e.KeyCode == Keys.Right)
+                RaumschiffBewegenRechts = false;
         }
     }
 }
